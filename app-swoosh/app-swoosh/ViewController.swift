57ef097b08dfddec517e555f9de24860f41e9178
Islam Gharib <islamgharib@Islams-MacBook-Pro.local>
2019-12-24 20:38:22 +0200

    creating app-swoosh and dragging assets images

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Islam Gharib on 12/24/19.
+//  Copyright © 2019 Gharib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
