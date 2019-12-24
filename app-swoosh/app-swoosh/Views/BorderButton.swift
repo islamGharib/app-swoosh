8e5002ef7542d4ce8010519f0426efa4581372d5
Islam Gharib <islamgharib@Islams-MacBook-Pro.local>
2019-12-24 21:34:40 +0200

    Creating the welcome screen

---
+++ app-swoosh/app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Islam Gharib on 12/24/19.
+//  Copyright © 2019 Gharib. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
