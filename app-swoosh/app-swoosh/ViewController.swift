aef1e3efe272963c025da74989ea63e73e642962
Islam Gharib <islamgharib@Islams-MacBook-Pro.local>
2019-12-26 19:47:28 +0200

    Working with frames

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    @IBOutlet weak var goAllLab: UILabel!
+    @IBOutlet weak var startedBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+                            // working with frames -> every frame has a position and size
+        swoosh.frame = CGRect(x: view.frame.size.width/2 - swoosh.frame.size.width/2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        
+                // 2 ways
+        //bgImg.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height:    )
+        // or
+        bgImg.frame = view.frame
+        
+        goAllLab.frame = CGRect(x: view.frame.size.width/2 - goAllLab.frame.size.width/2, y: 133, width: goAllLab.frame.size.width, height: goAllLab.frame.size.height)
+        
+        startedBtn.frame = CGRect(x: view.frame.size.width/2 - startedBtn.frame.size.width/2, y: view.frame.size.height - startedBtn.frame.size.height - 50, width: startedBtn.frame.size.width, height: startedBtn.frame.size.height)
+        
+        
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
