15ce37a848e5fbfc301d6460b038f9594b6ebc3c
Islam Gharib <islamgharib@Islams-MacBook-Pro.local>
2020-01-02 18:10:23 +0200

    Solving setValue:forUndefinedKey debugging exception

--- app-swoosh/app-swoosh/Controller/WelcomeVC.swift
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -9,11 +9,12 @@
 import UIKit
 
 class WelcomeVC: UIViewController {
-    
+    /*
     @IBOutlet weak var swoosh: UIImageView!
     @IBOutlet weak var bgImg: UIImageView!
     @IBOutlet weak var goAllLab: UILabel!
     @IBOutlet weak var startedBtn: BorderButton!
+    */
     
     override func viewDidLoad() {
         super.viewDidLoad()
