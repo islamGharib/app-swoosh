e6ba698b8d39ecf0e05de4a891962d18c86df2a6
Islam Gharib <islamgharib@Islams-MacBook-Pro.local>
2020-01-07 16:51:23 +0200

    Segues programmatically

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -16,10 +16,11 @@ class LeagueVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onNextTapped(_ sender: Any) {
+        // programmatic segue (segue from VC to another VC)
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
+    
     
 
     /*
